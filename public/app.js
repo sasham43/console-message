79b9052a9090e51482e5193640356bc983555835
sasham43 <sashasemail@gmail.com>
2017-02-20 18:18:43 -0600

    turning out messages

--- public/app.js
+++ public/app.js
@@ -1,5 +1,43 @@
-$(function(){
-  var console = window.setInterval(function(){
-    $(".cursor").toggleClass("hide");
-  }, 1000)
-})
+$(function () {
+  var cursor_blink = window.setInterval(function () {
+    $('.cursor').toggleClass('hide');
+  }, 500);
+
+  $('.go-button').on('click', function(){
+    var message = $('.message-input').val();
+    console.log(message);
+    displayMessage(message);
+  });
+
+  var count = 0;
+
+  var socket = io.connect('http://localhost:3000');
+
+  socket.on('connect', function () {
+    console.log('connected.');
+  });
+
+  socket.on('message', function(message){
+    console.log(message);
+    displayMessage(message);
+    // count++
+  });
+});
+
+function displayMessage(message){
+  var container = {};
+  $('.message-container').append('<div class="message"></div>');
+  // $('.message').append(message);
+  var wait_count = 0;
+  var count = 0;
+  for(var i = 0; i <= message.length; i++){
+    wait_count += 100;
+    window.setTimeout(function(){
+      // wait
+
+      $('.message').append(message.charAt(count));
+      // console.log('wait',message, message.charAt(temp), temp);
+      count++
+    },wait_count)
+  }
+}
--- server.js
+++ server.js
@@ -7,14 +7,19 @@ io.on('connection', function(client){
   client.on('event', function(data){});
   client.on('disconnect', function(){});
 
+  app.use('/message/:message', function(req, res){
+    console.log(req.params.message);
+    client.emit('message', req.params.message);
+    res.sendStatus(200);
+  });
 
 });
 
 app.use(express.static('public'));
 
-app.use('/', function(req, res){
-  res.sendFile('index.html');
-});
+// app.use('/', function(req, res){
+//   res.sendFile('index.html');
+// });
 
 server.listen(3000, function(){
   console.log('listening on 3000...');
